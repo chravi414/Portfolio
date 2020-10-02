d8f7c68132d938f7d0767a0c61861b11c4a60333
chravi414 <chravindra414@gmail.com>
2020-10-02 18:59:25 -0500

    Menu button changes

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const menuItems = document.querySelectorAll(".nav-item");
+
+console.log(menuItems);
+
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuBranding.classList.add("show");
+    menuNav.classList.add("show");
+    menuItems.forEach((item) => item.classList.add("show"));
+    showMenu = false;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuBranding.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuItems.forEach((item) => item.classList.remove("show"));
+    showMenu = true;
+  }
+}
